3283f97c12d5f64c466334ec904ec50b7250a3da
Lakiduck <lwalgama@hotmail.com>
2018-11-07 21:25:19 +1100

    updated main.css script.js and index.html

--- public/js/script.js
+++ public/js/script.js
@@ -5,4 +5,26 @@ window.onload = function(){
       message: 'This is where files are kept'
     }
   });
+
+  /*new Vue({
+    el: '#fileact',
+    methods: {
+      show: function(){
+        var fileInsert = document.createElement('input');
+        document.getElementId("files").appendChild(fileInsert);
+        fileInsert.attr('autocomplete', 'off');
+        fileInsert.attr('placeholder', 'Insert File Name Here');
+        fileInsert.attr('v-model', 'newTodo');
+        fileInsert.attr('@keyup.enter', 'addTodo');
+      }
+    }
+
+  });*/
+
+  document.getElementById('fileact').addEventListener('click', function(){
+      var fileInsert = document.createElement('input');
+      document.getElementById("files").appendChild(fileInsert);
+      $("#fileprompt").hide();
+  });
+
 }
